399fcfc3e1fa3a6f099c6e5738a8bf49feff2926
lvyafei <lvyafeixx@gmail.com>
2016-08-21 18:09:56 +0800

    依赖框架底层实现

--- src/main/java/com/fs/app/analysis/service/classify/ClassifyService.java
+++ src/main/java/com/fs/app/analysis/service/classify/ClassifyService.java
@@ -2,46 +2,63 @@ package com.fs.app.analysis.service.classify;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.json.JSONArray;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import com.fs.app.analysis.utils.BosonApiHelper;
 import com.fs.app.analysis.utils.HtmlParse;
+import com.fs.commons.app.pojo.NewsPojo;
+import com.fs.commons.crawler.service.INewsService;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
 public class ClassifyService {
-
-	public static String myapitoken="UuES7ofn.8947.LFKgPfbL_qUS";
-	public static String classifyurl="http://api.bosonnlp.com/classify/analysis";
 	
-	public static String getClassify(List<String> contents){
+	@Autowired
+	private INewsService newsService;
+	
+	public List<NewsPojo> updateClassify(List<NewsPojo> news){
+		List<String> contents=new ArrayList<String>();
+		for (NewsPojo pojo : news) {
+			contents.add(HtmlParse.htmltotext(pojo.getContext()));
+		}
 		try {
 			String str=com.alibaba.fastjson.JSONArray.toJSONString(contents);
 			HttpResponse<JsonNode> Response = Unirest
-				    .post(classifyurl)
+				    .post(BosonApiHelper.classifyurl)
 				    .header("Accept", "application/json")
 				    .header("Content-Type", "application/json")
-				    .header("X-Token", myapitoken)
+				    .header("X-Token", BosonApiHelper.myapitoken)
 				    .body(str)
 				    .asJson();
 			JsonNode node=Response.getBody();
 			JSONArray jsonarray= node.getArray();
 			for(int i=0;i<contents.size();i++){
 				String cls=jsonarray.get(i).toString();
-				System.out.println("分类结果:"+cls);
+				news.get(i).setCategory(cls);
 			}
 		} catch (Exception e) {
 			e.printStackTrace(); 
 		}
-		return "";
+		for (NewsPojo pojo : news) {
+			newsService.updateNewsCategory(pojo,pojo.getCategory());
+		}
+		return news;
 	}
 	
-	public static void main(String[] args){
-		List<String> result=new ArrayList<String>();
-		result.add("我们都是中国人");
-		result.add("我们都是日本人");
-		result.add(HtmlParse.htmltotext("<p>俄否决安理会谴责叙军战机空袭阿勒颇平民</p>"));
-		getClassify(result);
+	public boolean initClassify(){
+		List<NewsPojo> pojos= newsService.getNewsForCategory("");
+		List<NewsPojo> tmppojos=new ArrayList<NewsPojo>();
+		int index=0;
+		for (NewsPojo newsPojo : pojos) {
+			tmppojos.add(newsPojo);
+			index++;
+			if(index>=40){
+				updateClassify(tmppojos);
+				index=0;
+				tmppojos=new ArrayList<NewsPojo>();
+			}
+		}
+		return true;
 	}
 }
---
+++ src/main/java/com/fs/app/analysis/utils/BosonApiHelper.java
@@ -0,0 +1,8 @@
+package com.fs.app.analysis.utils;
+
+public class BosonApiHelper {
+
+	public static String myapitoken="UuES7ofn.8947.LFKgPfbL_qUS";
+	
+	public static String classifyurl="http://api.bosonnlp.com/classify/analysis";
+}
